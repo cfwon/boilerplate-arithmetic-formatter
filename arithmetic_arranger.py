aee32899b5c8dbf8d3d62e794ea544f0e5c191f8
Aaron Wong <wongchifung831@gmail.com>
2023-09-11 00:06:42 +0800

    test

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,11 @@
 def arithmetic_arranger(problems):
-
-
+    # Justify len(problem) > 5, then return Too many problem
+    if len(problems) > 5:
+        return "Error: Too many problems."
+    str
+    # 1. Separate list
+    for lst in problems:
+    # 2. put splitted variable into line
+    
+        
     return arranged_problems
